67ae315fa4a48eb4b4f7ebca5a4b1b767a189acc
vpfl95 <k_dh610@naver.com>
2023-03-04 02:51:04 +0900

    first

---
+++ src/main/java/com/dh/home/TimeDealServerApplication.java
@@ -0,0 +1,13 @@
+package com.dh.home;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeDealServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimeDealServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dh/home/TimeDealServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dh.home;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimeDealServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
